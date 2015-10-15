b2cc9fc10d4c95c71bf65e9ef598132a5695a8b3
Richo <richo2192000@gmail.com>
2015-10-15 21:46:18 +0800

    Do several url testing.

--- spec/collections_spec.rb
+++ spec/collections_spec.rb
@@ -2,12 +2,15 @@ require 'yaml'
 require 'minitest/autorun'
 require_relative '../lib/taaze.rb'
 
-URL = ''
+URL = ['http://www.taaze.tw/container_zekeaclt_view.html?ci=12522728', 'http://www.taaze.tw/container_zekeaclt_view.html?ci=13193872']
 collections_from_file = YAML.load(File.read('./spec/testfiles/collections.yml'))
-collections_found = Taaze.new(URL).collections
 
-describe "Get all the collections with specific user's url" do
-  it 'has the right number of collections' do
-    collections_found.size.must_equal collections_from_file[URL].size
+URL.each do |url|
+  collections_found = Taaze.new(url).collections
+
+  describe "Get all the collections with specific user's url" do
+    it 'has the right number of collections' do
+      collections_found.size.must_equal collections_from_file[url].size
+    end
   end
 end
