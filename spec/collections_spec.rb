17f1e9c699b62eec030b2e750a10de590e9f3f48
Richo <richo2192000@gmail.com>
2015-10-15 20:24:00 +0800

    + Change test file name + Add basic test codes

---
+++ spec/collections_spec.rb
@@ -0,0 +1,13 @@
+require 'yaml'
+require 'minitest/autorun'
+require_relative '../lib/taaze.rb'
+
+URL = ''
+collections_from_file = YAML.load(File.read('./spec/testfiles/collections.yml'))
+collections_found = Taaze.new(url).collections
+
+describe "Get all the collections with specific user's url" do
+  it 'has the right number of collections' do
+    collections_found.size.must_equal collections_from_file[URL].size
+  end
+end
