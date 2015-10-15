d4087a51427ba4274b9072ea16f3e3caeeca1d47
Richo <richo2192000@gmail.com>
2015-10-15 21:46:18 +0800

    Do several url testing.

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -2,12 +2,15 @@ require 'yaml'
 require 'minitest/autorun'
 require_relative '../lib/taaze.rb'
 
-URL = ''
+URL = ['http://www.taaze.tw/container_zekeaclt_view.html?ci=12522728', 'http://www.taaze.tw/container_zekeaclt_view.html?ci=13193872']
 comments_from_file = YAML.load(File.read('./spec/testfiles/comments.yml'))
-comments_found = Taaze.new(url).comments
 
-describe "Get all the comments with specific user's url" do
-  it 'has the right number of comments' do
-    comments_found.size.must_equal comments_from_file[URL].size
+URL.each do |url|
+  comments_found = Taaze.new(url).comments
+
+  describe "Get all the comments with specific user's url" do
+    it 'has the right number of comments' do
+      comments_found.size.must_equal comments_from_file[url].size
+    end
   end
 end
