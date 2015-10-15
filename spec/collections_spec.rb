83959e43cd9f2732c1141b286e9b9e104035acd8
Richo <richo2192000@gmail.com>
2015-10-15 21:28:32 +0800

    Debugging

--- spec/collections_spec.rb
+++ spec/collections_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/taaze.rb'
 
 URL = ''
 collections_from_file = YAML.load(File.read('./spec/testfiles/collections.yml'))
-collections_found = Taaze.new(url).collections
+collections_found = Taaze.new(URL).collections
 
 describe "Get all the collections with specific user's url" do
   it 'has the right number of collections' do
