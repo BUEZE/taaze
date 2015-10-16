80866f4479f476a87a053d8a1a132891fa8bb9d9
Richo <richo2192000@gmail.com>
2015-10-16 15:09:26 +0800

    Change url to user_id.

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -2,15 +2,15 @@ require 'yaml'
 require 'minitest/autorun'
 require_relative '../lib/taaze.rb'
 
-URL = ['http://www.taaze.tw/container_zekeaclt_view.html?ci=12522728', 'http://www.taaze.tw/container_zekeaclt_view.html?ci=13193872']
+URL = %w(12522728 13193872)
 comments_from_file = YAML.load(File.read('./spec/testfiles/comments.yml'))
 
-URL.each do |url|
-  comments_found = Taaze::TaazeComments.new(url).comments
+USER_ID.each do |user_id|
+  comments_found = Taaze::TaazeComments.new(user_id).comments
 
   describe "Get all the comments with specific user's url" do
     it 'has the right number of comments' do
-      comments_found.size.must_equal comments_from_file[url].size
+      comments_found.size.must_equal comments_from_file[user_id].size
     end
   end
 end
