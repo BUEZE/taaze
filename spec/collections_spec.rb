c75ef0529b02686a2c04cdae522a17058acc0b8f
Richo <richo2192000@gmail.com>
2015-10-17 23:43:53 +0800

    Use vcr on tests.

--- spec/collections_spec.rb
+++ spec/collections_spec.rb
@@ -12,18 +12,20 @@ VCR.configure do |config|
   config.hook_into :webmock
 end
 
-USER_ID.each do |user_id|
-  collections_found = Taaze::TaazeCollections.new(user_id).collections
+VCR.use_cassette('collections') do
+  USER_ID.each do |user_id|
+    collections_found = Taaze::TaazeCollections.new(user_id).collections
 
-  describe "Get all the collections with specific user's id" do
-    it 'has the right number of collections' do
-      collections_found.size.must_equal collections_from_file[user_id].size
-    end
+    describe "Get all the collections with specific user's id" do
+      it 'has the right number of collections' do
+        collections_found.size.must_equal collections_from_file[user_id].size
+      end
 
-    collections_found.map do |book|
-      it 'finds book in the test collection' do
-        exist = collections_from_file[user_id].include? book
-        [book, exist].must_equal [book, true]
+      collections_found.map do |book|
+        it 'finds book in the test collection' do
+          exist = collections_from_file[user_id].include? book
+          [book, exist].must_equal [book, true]
+        end
       end
     end
   end
