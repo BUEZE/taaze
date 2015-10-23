3c58125b7a15d41465d2a049b79b7eff0a5d3729
Hiraku <dev@hiraku.tw>
2015-10-23 11:55:58 +0800

    Add specs

--- spec/tags_spec.rb
+++ spec/tags_spec.rb
@@ -4,7 +4,7 @@ require 'vcr'
 require 'webmock/minitest'
 require_relative '../lib/taaze.rb'
 
-BOOK_ID = %w(11100745134 11100714084 11303096752)
+BOOK_ID = %w(11100763435 11100745134 11100714084 11303096752)
 tags_from_file = YAML.load(File.read('./spec/fixures/tags.yml'))
 
 VCR.configure do |config|
@@ -15,7 +15,6 @@ end
 VCR.use_cassette('tags') do
   BOOK_ID.each do |book_id|
     tags_found = Taaze::TaazeBooktags.new(book_id).tags
-
     describe "Get all the tags with specific book's id" do
       it 'has the right number of tags' do
         tags_found.size.must_equal tags_from_file[book_id].size
@@ -23,7 +22,7 @@ VCR.use_cassette('tags') do
 
       tags_found.map do |tag|
         it 'finds tags in the test tags' do
-          exist = tags_found[book_id].include? tag
+          exist = tags_from_file[book_id].include? tag
           [tag, exist].must_equal [tag, true]
         end
       end
